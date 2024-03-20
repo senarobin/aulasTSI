dc16f0f11ee0d6b712b80fbb5413390ab1b867e9
= <luciano.cnrn@gmail.com>
2024-03-20 16:16:14 -0300

    Atualizando projeto

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,61 @@
-var nome = prompt("Qual seu nome?");
+var mes = prompt("Digite o mês:" 
+                    + "\n 1 - Janeiro" 
+                    + "\n 2 - Fevereiro"
+                    + "\n 3 - Março");
 
-document.getElementById("descricao").innerHTML += nome;
+if(mes == 1){
+    alert("Janeiro");
+} else if(mes == 2){
+    alert("Fevereiro");
+} else if(mes == 3){
+    alert("Março");
+} else {
+    alert("Mês inválido");
+}                  
+
+switch(mes){
+    case '1': 
+        alert("Janeiro");
+        break;
+    case '2':
+        alert("Fevereiro");
+        break;
+    case '3':
+        alert("Março");
+        break;
+    default:
+        alert("Mês inválido");
+        break;
+}
+
+/*
+
+var preco = prompt("Qual o preço do produto?");
+var preco = 20;
+var preco2 = "20";
+
+console.log("Preço:" + typeof preco);
+console.log("Preço2:" + typeof preco2);
+
+if(preco !== preco2){
+    alert("Barato...");
+} else {
+    alert("Caro...");
+}
+
+var mensagem = (preco <= 20) ? "Barato..." : "Caro...";
+
+alert(mensagem);
+
+
+var nome = prompt("Qual o seu nome?");
+
+var resposta = confirm(nome + ", você gostaria de continuar?");
+
+if(resposta){
+    alert("Bem Vindo...");
+} else {
+    window.location.href = "../index.html";
+}
+
+*/
